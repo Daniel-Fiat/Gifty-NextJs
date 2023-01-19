a115ea2295b9f33fa9a7bd030dbc3df83ae80fc0
Daniel-Fiat <danielfiatcasado@gmail.com>
2023-01-19 01:19:22 +0100

    ProductDetail page falta configuracion de auth context

---
+++ pages/product/[id].jsx
@@ -0,0 +1,146 @@
+import Link from "next/link"
+import { useContext, useEffect, useState } from "react";
+import { useRouter } from 'next/router'
+import Image from "next/image";
+
+
+import ReviewAPI from '../../services/review.service'
+import ProductAPI from '../../services/product.service'
+import UserApi from '../../services/user.service'
+
+import wishTrue from '../../public/assets/Corazon-rojo.png';
+import wishFalse from '../../public/assets/Corazon-Blanco.png';
+import startRanting from '../../public/assets/StarRating.png';
+
+import Nabvar from '../../components/Navbar/Navbar'
+
+
+
+const ProductDetail = () => {
+    const [validateWishList, setValidateWishList] = useState()
+    const [product, setProduct] = useState({})
+    const [reviews, setReviews] = useState()
+    const router = useRouter()
+    useEffect(() => {
+
+        ProductAPI.getOneProduct(router.query.id)
+            .then(productRes => {
+                return productRes
+            })
+            .then((productRes) => {
+                setProduct(productRes)
+                if (false) {
+                    UserApi.getOne(user._id).then(userApi => {
+                        const newvalidate = userApi.wishList.includes(productRes._id)
+                        setValidateWishList(newvalidate)
+                    })
+                }
+            })
+    }, [])
+
+    useEffect(() => {
+        ReviewAPI.getByProduct(router.query.id).then(reviews => {
+            setReviews(reviews)
+
+        })
+
+    }, [])
+
+    const removeWishList = (event) => {
+        event.preventDefault()
+        UserApi.removeWishList(user._id, id).then()
+        const newvalidate = false
+        setValidateWishList(newvalidate)
+    }
+
+    const addWishList = (event) => {
+        event.preventDefault()
+        UserApi.addWishList(user._id, id).then()
+        const newvalidate = true
+        setValidateWishList(newvalidate)
+    }
+
+    return (
+        <>
+            {
+
+                <div id="ProductCard">
+                    <h1 id="titleCard">{product.name}</h1>
+                    <figure id='figure-imgProduct'>
+                        <img id="IMGproduct" src={product.imgUrl} alt="esto" />
+                        <figcaption>
+                            {validateWishList ?
+                                (<form onSubmit={removeWishList}>
+                                    <button type="submit">
+                                        <Image
+                                            src={wishTrue}
+                                            alt={wishTrue}
+                                            height={30}
+                                            width={100} />
+                                    </button>
+                                </form>)
+                                :
+                                (<form onSubmit={addWishList}>
+                                    <button type="submit">
+                                        <Image
+                                            src={wishFalse}
+                                            alt={wishFalse}
+                                            height={30}
+                                            width={100} />
+                                    </button>
+                                </form>)
+
+                            }
+                        </figcaption>
+                    </figure>
+                    <p id='DetailsProduct' >{product.description}</p>
+                    <h1>                                        <Image
+                        src={startRanting}
+                        alt={wishTrue}
+                        id="StartRating"
+                        height={20}
+                        width={17} />{product.rating}</h1>
+                    <span id='PriceSpan'>{`${product.price} €`}</span>
+                </div>
+
+            }
+            <div id='Gift-boton'>
+                <Link href={`/gifty/${product._id}`}>
+                    Regalar
+                </Link>
+            </div>
+            {reviews && <h5>reviews</h5>}
+            {reviews?.map(review => {
+                return (
+                    <div key={review._id}>
+                        <div class="card">
+                            <div class="card-header">
+                                {"⭐".repeat(review.rating)}
+                            </div>
+                            <div class="card-body">
+                                <h5 class="card-title">{review.userId.email}</h5>
+                                <p class="card-text">{review.comment}</p>
+                                <small> {review.createdAt}</small>
+                            </div>
+                        </div>
+                    </div>
+                )
+            })}
+
+            <Nabvar />
+        </>
+    );
+
+}
+
+export default ProductDetail;
+
+export async function getServerSideProps() {
+    const products = await ProductAPI.getTopSix()
+
+    return {
+        props: {
+            products
+        }
+    }
+}
